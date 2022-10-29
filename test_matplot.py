22e0ab4e790cc6e635174b49ae9ca93903eb96f7
xmcbbkad <188135446@qq.com>
2022-10-29 20:53:48 +0800

    new

---
+++ test_matplot.py
@@ -0,0 +1,5 @@
+import mplfinance as mpf
+import pandas as pd
+daily = pd.read_csv('/Users/xiaokunfan/programme_trading/data/taobao_TSLA/2022-02-25.csv',index_col=0,parse_dates=True)
+daily.index.name = 'Date'
+mpf.plot(daily,type='candle',mav=(3,6,9),volume=True,show_nontrading=True)
